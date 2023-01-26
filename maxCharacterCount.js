0165d1d711bd05b3edc65bc7e1e43426515de808
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2023-01-26 17:04:48 +0600

    Max character count - Common interview question

---
+++ maxCharacterCount.js
@@ -0,0 +1,24 @@
+function max(str) {
+    const hashMap = {};
+    let max = 0;
+    let maxChar = '';
+
+    for(let char of str) {
+        if(char in hashMap) {
+            hashMap[char] = hashMap[char] + 1;
+        }
+        else {
+            hashMap[char] = 1;
+        }
+    }
+    for(let char in hashMap) {
+        if(hashMap[char] > max) {
+            max = hashMap[char];
+            maxChar = char;
+        }
+    }
+    console.log(hashMap)
+    return maxChar;
+}
+
+console.log(max('apple'));
