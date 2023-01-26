b88e0f9d3c6c808e96377c94a4f18e910d2bd4e0
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2023-01-26 12:32:36 +0600

    Leetcode - FizzBuzz

---
+++ FizzBuzz.js
@@ -0,0 +1,21 @@
+function fizzBuzz(number) {
+    let result = [];
+
+    for(let i = 1; i <= number; i++) {
+        if (i % 3 === 0 && i % 5 === 0){
+            result.push("FizzBuzz");
+        }
+        else if (i % 3 ===0) {
+            result.push("Fizz");
+        }
+        else if (i % 5 === 0) {
+            result.push("Buzz");
+        }
+        else {
+            result.push(i.toString());
+        }
+    }
+    return result;
+}
+
+console.log(fizzBuzz(15))
